b6cabcbe205a96c9e4a5a7cff38eab769475d24b
shayanfirouzian <100245484+shayanfirouzian@users.noreply.github.com>
2024-05-11 05:18:17 +0330

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+  
+setuptools.setup( 
+    name='Trapezoidal_Fuzzy_Number',
+    version='1.0',
+    author='Shayan Firouzian',
+    packages=['Trapezoidal_Fuzzy_Number'],
+    description='A library to work with trapezoidal fuzzy numbers',
+    long_description_content_type="text/markdown", 
+    url="https://github.com/shayanfirouzian", 
+    license='MIT', 
+    python_requires='>=3.8', 
+    install_requires=[] 
+) 
