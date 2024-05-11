fa9861e6075ebb5f88bba95792be1d5c406e19b3
shayanfirouzian <100245484+shayanfirouzian@users.noreply.github.com>
2024-05-11 07:21:15 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
   
 setuptools.setup( 
     name='Trapezoidal_Fuzzy_Number',
-    version='1.0',
+    version='1.1',
     author='Shayan Firouzian',
     packages=['Trapezoidal_Fuzzy_Number'],
     description='A library to work with trapezoidal fuzzy numbers',
